5c6cc6542aeef34bbbe54cdbdc7c7ff8da02442e
viduraakalanka <37825080+viduraakalanka@users.noreply.github.com>
2019-04-27 13:45:11 +0530

    HR python solutions

---
+++ Python/4-division.py
@@ -0,0 +1,8 @@
+from __future__ import division
+#__future__ is a pseudo-module which programmers can use to enable new language features which are not compatible with the current interpreter. 
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print(a//b)
+    print(a/b)
