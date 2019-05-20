55cbde0acf2316f379ea1e16d3cd4d6c8819cb59
viduraakalanka <37825080+viduraakalanka@users.noreply.github.com>
2019-05-20 17:19:09 +0530

    HR C Solutions

--- C/16 Digit Frequency.c
+++ C/16 Digit Frequency.c
@@ -8,21 +8,21 @@ int main() {
     char *word ;
     int   is_num;
     word = (char*)malloc(1024*sizeof(char));
-    
+
     scanf("%s", word);
     word = realloc(word, strlen(word) + 1);
 
-    int counter_list [10] = {0,0,0,0,0,0,0,0,0,0};  
-    
+    int counter_list [10] = {0,0,0,0,0,0,0,0,0,0};
+
     int length = strlen(word);
-    
+
     for(int i=0; i<length; i++){
         //printf("%c",word[i]);
-        is_num = word[i]-'0'; 
+        is_num = word[i]-'0';
         for (int j=0; j<10; j++){
-            
+
             if(is_num==j){
-                counter_list[j] +=1;
+                counter_list[j] +=1;a
             }
 
         }
@@ -41,17 +41,17 @@ int main() {
 int main() {
     int* nums = (int*) malloc(10 * sizeof(int));
     char c;
-    
+
     for(int i = 0; i < 10; i++)
         *(nums+i) = 0;
 
     while(scanf("%c", &c) == 1)
         if(c >= '0' && c <= '9')
             (*(nums+(c-'0')))++;
-    
+
     for(int i = 0; i < 10; i++)
         printf("%d ", *(nums+i));
-    
+
     return 0;
 }
 
