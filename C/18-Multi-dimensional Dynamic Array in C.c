4a6af791362abae12ab8f234d4ba261ce790a7a0
Vidura Akalanka <37825080+viduraakalanka@users.noreply.github.com>
2019-05-23 12:27:45 +0530

    Update 18-Multi-dimensional Dynamic Array in C.c

--- C/18-Multi-dimensional Dynamic Array in C.c
+++ C/18-Multi-dimensional Dynamic Array in C.c
@@ -47,3 +47,29 @@ int main()
             //*(*(total_number_of_pages+x)+*(total_number_of_books+x)-1)=y;
 
         } else if (type_of_query == 2) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            printf("%d\n", *(*(total_number_of_pages + x) + y));
+        } else {
+            int x;
+            scanf("%d", &x);
+            printf("%d\n", *(total_number_of_books + x));
+        }
+    }
+
+    if (total_number_of_books) {
+        free(total_number_of_books);
+    }
+    
+    for (int i = 0; i < total_number_of_shelves; i++) {
+        if (*(total_number_of_pages + i)) {
+            free(*(total_number_of_pages + i));
+        }
+    }
+    
+    if (total_number_of_pages) {
+        free(total_number_of_pages);
+    }
+    
+    return 0;
+}
