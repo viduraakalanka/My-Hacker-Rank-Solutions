eb172db1629ac87c85fae19bc967ed9c4828752d
viduraakalanka <37825080+viduraakalanka@users.noreply.github.com>
2019-05-26 07:45:39 +0530

    HR Java solutions

---
+++ Java/02 - Java If Else.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+
+        if (N%2==1){
+            System.out.println("Weird");
+        }
+        else if (N==2 || N==4){
+            System.out.println("Not Weird");
+        }
+        else if (6<= N && N<=20 && N%2 == 0){
+            //cant put expressions like a<b<c. it should be a<b && b<c
+            System.out.println("Weird");
+        }
+        else if (N>20 && N%2==0){
+            System.out.println("Not Weird");
+        }
+    }
+}
