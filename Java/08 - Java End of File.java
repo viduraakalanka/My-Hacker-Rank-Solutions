7254583e4ab45b0f0d19a7226beee77aefc97052
Vidura Akalanka <37825080+viduraakalanka@users.noreply.github.com>
2019-08-08 15:44:03 +0530

    Create 08 - Java End of File.java

---
+++ Java/08 - Java End of File.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        String str1;
+
+        while (sc.hasNext()){
+            i = i+1;
+            str1 = sc.nextLine();
+            System.out.printf("%d "+str1+ "\n",i);
+        }
+    }
+}
+
