08bc89bd813641b6d16090e77be30962ac5ef371
Vidura Akalanka <37825080+viduraakalanka@users.noreply.github.com>
2020-07-12 11:59:51 +0530

    Upload

---
+++ C++/01-Hello World/01-Hello World .cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!\n");
+    //puts("Hello, World!"); //from cstdio
+    cout << "Hello, World!" << endl; //from iostream
+    return 0;
+}
---
+++ C++/02-Input and Output/02-Input and Output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Read input from STDIN. Print output to STDOUT */
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    int sum = a+b+c+d+e;
+    cout << sum << endl;
+    return 0;
+}
