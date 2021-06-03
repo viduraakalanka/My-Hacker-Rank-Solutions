5be41f25b60f3d00ea47369c3609d73567e80810
Vidura Akalanka <vidura2013@live.com>
2021-06-03 08:44:53 +0530

    Python

---
+++ Algorithms/4-Dynamic Programming - Coin Change Problem.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getWays' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. LONG_INTEGER_ARRAY c
+#
+
+def getWays(n, c):
+    ways = [0] * (n + 1);
+
+    ways[0] = 1;
+ 
+    for i in range(len(c)):
+        for j in range(len(ways)):
+            if (c[i] <= j):
+                ways[j] += ways[(int)(j - c[i])];
+ 
+    return ways[n];
+ 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    ways = getWays(n, c)
+
+    fptr.write(str(ways) + '\n')
+
+    fptr.close()
