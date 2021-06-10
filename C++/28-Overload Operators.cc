f7e4c2769e17c883d4d1bbcdb6c7bb2140431bf8
Vidura Akalanka <vidura2013@live.com>
2021-06-10 12:15:14 +0530

    c++

---
+++ C++/28-Overload Operators.cc
@@ -0,0 +1,58 @@
+//Operator Overloading
+
+#include<iostream>
+
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+    }
+};
+
+Complex operator + (Complex const &obj1, Complex const &obj2) {
+    Complex res;
+    res.a = obj1.a + obj2.a;
+    res.b = obj1.b + obj2.b;
+    return res;
+}
+    
+ostream& operator <<(ostream& out,Complex const& obj){
+    out << obj.a;
+    out << "+i" << obj.b << endl;
+    return out;
+}
+
+int main()
+{
+    Complex x,y;
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    x.input(s1);
+    y.input(s2);
+    Complex z=x+y;
+    cout<<z<<endl;
+}
