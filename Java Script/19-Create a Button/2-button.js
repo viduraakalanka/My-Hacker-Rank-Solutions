5da6d7f3bbe8e0f54acbdf8ef74bde2c89c458bd
Vidura Akalanka <37825080+viduraakalanka@users.noreply.github.com>
2022-02-17 19:35:02 +0530

    Create 2-button.js

---
+++ Java Script/19-Create a Button/2-button.js
@@ -0,0 +1,9 @@
+ /* Get the element with id='btn' */
+var htmlButton = document.getElementById('btn');
+
+
+/* This sets the action to perform on a click event */
+btn.onclick = function() {
+    /* This changes the button's label */
+    btn.innerHTML++; 
+};
