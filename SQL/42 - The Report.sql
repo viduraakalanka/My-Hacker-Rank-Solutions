8f98ca136f3fd5fd70e1ba33622d55f1a5eed667
Vidura Akalanka <37825080+viduraakalanka@users.noreply.github.com>
2023-01-27 08:38:13 +0530

    Update 42 - The Report.sql

--- SQL/42 - The Report.sql
+++ SQL/42 - The Report.sql
@@ -1,9 +1,13 @@
-SELECT CASE WHEN Grades.Grade <8 
-            THEN NULL ELSE Students.Name END,
-Grades.Grade, Students.Marks
+SELECT (CASE WHEN Grades.Grade <8 
+             THEN NULL 
+             ELSE Students.Name 
+        END),
+    Grades.Grade, Students.Marks
 FROM Students
 LEFT JOIN Grades
 ON Students.Marks BETWEEN Grades.Min_Mark AND Grades.Max_Mark
 ORDER BY Grades.Grade DESC,
-CASE WHEN Grades.Grade >= 8 THEN Students.Name
-ELSE Students.Marks END ASC;
+    (CASE WHEN Grades.Grade >= 8 THEN Students.Name
+          ELSE Students.Marks 
+          END) 
+    ASC;
